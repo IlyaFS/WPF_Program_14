601c9f48b48b89ea9fc820600683d64dd74c82d4
Илья Филиппов <filippov.i.s@gmail.com>
2022-02-12 18:55:33 +0300

    Добавьте файлы проекта.

---
+++ WPF_Program_14/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Program_14
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Product> Products;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Products = new List<Product>();
+
+            Products.Add(new Product()
+            {
+                Name = "Картошка",
+                Price = 120,
+                Category = CategoryTypes.Food,
+                Image = "Image/1.jpg"
+
+            });
+            Products.Add(new Product()
+            {
+                Name = "Холодильник",
+                Price = 80000,
+                Category = CategoryTypes.Tech,
+                Image = "Image/2.jpg"
+
+            });
+            List1.ItemsSource = Products;
+        }
+    }
+}
---
+++ WPF_Program_14/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Program_14
+{
+    public enum CategoryTypes
+    {
+        Food,
+        Tech
+    }
+
+    public class Product
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public CategoryTypes Category { get; set; }
+        public string Image { get; set; }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
